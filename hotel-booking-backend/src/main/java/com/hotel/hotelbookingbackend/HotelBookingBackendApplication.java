398dc6d672e8c20f20cfe4576492b5c4514b9f82
Thủy Nguyễn <thuynvm1610@gmail.com>
2026-02-14 00:10:27 +0700

    Replace entire project

---
+++ hotel-booking-backend/src/main/java/com/hotel/hotelbookingbackend/HotelBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.hotelbookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingBackendApplication.class, args);
+    }
+
+}
---
+++ hotel-booking-backend/src/test/java/com/hotel/hotelbookingbackend/HotelBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.hotelbookingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
