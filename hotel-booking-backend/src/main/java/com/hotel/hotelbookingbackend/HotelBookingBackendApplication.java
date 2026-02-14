a1b4c777160b7252e0a05623fdc7507b9e6ec44c
Thủy Nguyễn <thuynvm1610@gmail.com>
2026-02-14 08:20:45 +0700

    hotel-booking-admin, REUPLOADED

--- hotel-booking-backend/src/main/java/com/hotel/hotelbookingbackend/HotelBookingBackendApplication.java
+++ hotel-booking-backend/src/main/java/com/hotel/hotelbookingbackend/HotelBookingBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HotelBookingBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HotelBookingBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingBackendApplication.class, args);
+	}
 
 }
